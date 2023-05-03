172511e7ae7684bdca1a71c605d9b6bd44798e9c
Kirill Yakovlev <yakovlev1.kirill@yandex.ru>
2023-05-03 17:08:56 +0300

    Init my app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <h1>Hello, React-Sushi!</h1>
+            </div>
+        </div>
+    );
+};
+
+export default App;
+
